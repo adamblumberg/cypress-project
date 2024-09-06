5c9632f73fce6df37760af7ca1a675b70a48420e
Adam Blumberg <blumberg.adam@gmail.com>
2024-09-06 12:33:38 -0700

    New checkout functionality
    changes to shopping cart spec

--- cypress/e2e/ShoppingCart.spec.js
+++ cypress/e2e/ShoppingCart.spec.js
@@ -1,5 +1,42 @@
-describe("Recording 9/4/2024 at 10:29:50 AM", () => {
-  it("tests Recording 9/4/2024 at 10:29:50 AM", () => {
+describe("Guest Checkout: Shopping Cart Functionality", () => {
+  it("Test 1: As a Guest, I can search for and add items to the cart", () => {
+    //set the screen size
+    cy.viewport(2544, 732);
+    //visit the homepage
+    cy.visit("https://ecommerce-playground.lambdatest.io/index.php?route=common/home");
+    // Click on the search field
+    cy.get('input[name="search"]').eq(0).click();
+    // Type 'macbook pro' in the search field and press enter
+    cy.get('input[name="search"]').eq(0).type('macbook pro{enter}');
+    cy.wait(500)
+    // Assert the search worked by checking URL and image availability
+    cy.url().should('include', 'search=macbook+pro');
+    cy.get('img[alt="MacBook Pro"]').should('exist');
+    // Click the item (macbook pro)
+    cy.get('a[href="https://ecommerce-playground.lambdatest.io/index.php?route=product/product&product_id=62&search=macbook+pro"').click();
+    // Click the Add to Cart button
+    cy.get('button[title="Add to Cart"]').eq(1)
+    //cy.contains('button', 'Add to Cart').eq(1)
+
+
+
+    cy.get("#entry_217968 div.swiper-slide-active img").click();
+    cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=20");
+    cy.get("#entry_212408 > div > div:nth-of-type(1) div.active > img").click();
+    cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=product/product&path=20&product_id=28");
+    cy.get("#entry_216842 > button").click();
+    cy.get("#notification-box-top div:nth-of-type(1) > a").click();
+    cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/cart");
+    cy.get("#checkout-cart form input").click();
+    cy.get("#checkout-cart form input").type("2");
+    cy.get("form button.btn-primary").click();
+    cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/cart");
+    cy.get("#container a.btn-primary").click();
+    cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/checkout");
+  });
+
+  
+  it("As a Guest, I can add items to the cart", () => {
     cy.viewport(2544, 732);
     cy.visit("https://ecommerce-playground.lambdatest.io/index.php?route=common/home");
     cy.get("#entry_217968 div.swiper-slide-active img").click();
@@ -15,6 +52,10 @@ describe("Recording 9/4/2024 at 10:29:50 AM", () => {
     cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/cart");
     cy.get("#container a.btn-primary").click();
     cy.location("href").should("eq", "https://ecommerce-playground.lambdatest.io/index.php?route=checkout/checkout");
+  });
+
+  //Test 2: Guest Checkout
+  it("Test 2: As a Guest, I can check out ", () => {
     cy.get("#input-payment-firstname").click();
     cy.get("#input-payment-firstname").type("test");
     cy.get("#input-payment-lastname").type("test");
