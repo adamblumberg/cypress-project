cfe9bc915c8a23b05df355934adb2c7d9f2d1068
Adam Blumberg <blumberg.adam@gmail.com>
2025-07-21 10:06:19 -0700

    New folders for each site
    Added new folders for Demoqa, ecommerce-playground, and heroku

--- cypress/e2e/GameChangerBasicNav.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Basic Site Accessibility', () => {
-    it('Loads the home page successfully', () => {
-      cy.visit('https://web.gc.com/teams');
-      cy.get('header').should('be.visible');
-      cy.get('footer').should('be.visible');
-      cy.contains('Welcome').should('be.visible');
-    });
-  });
